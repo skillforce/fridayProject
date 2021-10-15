a01c06b45efcb9a73272017499ea69b4d89a9606
skillforce@mail.ru <skillforce@mail.ru>
2021-10-15 19:07:12 +0300

    Registration branch

--- src/main/ui/App.tsx
+++ src/main/ui/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './header/header';
 import {Routes} from './routes/Routes';
 
+//Registration branch
+
 const App = () => {
     return (
         <div className="App">
