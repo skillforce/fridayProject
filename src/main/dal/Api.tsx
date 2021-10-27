b3c3a197ecf1ffe290e9a4b116ad2e2065f4a50c
skillforce@mail.ru <7401642Denis>
2021-10-27 15:47:22 +0300

    to merge

--- src/main/dal/Api.tsx
+++ src/main/dal/Api.tsx
@@ -5,7 +5,7 @@ const settings = {
     withCredentials: true,
 }
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     ...settings
 })
 
